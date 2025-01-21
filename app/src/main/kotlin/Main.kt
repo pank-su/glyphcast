4285b9483beb3165071834d531936b45e48decbe
pank-su <frostkslo1@yandex.ru>
2025-01-21 21:15:03 +0300

    :lipstick: передвижение всего окна + :fire: isVisible

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -1,32 +1,31 @@
 package su.pank.exhelp.app
 
 
-import androidx.compose.foundation.ContextMenuArea
-import androidx.compose.foundation.ContextMenuItem
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.window.WindowDraggableArea
 import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ContentCopy
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.key
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import su.pank.exhelp.*
 
 
 fun main() {
@@ -35,10 +34,6 @@ fun main() {
 
     return application {
         val windowState = rememberWindowState(size = DpSize(300.dp, 50.dp))
-        var isVisible by remember {
-            mutableStateOf(true)
-        }
-
 
 
         Window(
@@ -48,78 +43,36 @@ fun main() {
             state = windowState,
             resizable = false,
             alwaysOnTop = true,
-            focusable = false,
-            visible = isVisible,
-            onKeyEvent = {
-                // эта возмо
-                when (it.key) {
-                    Key.DirectionRight -> {
-                        windowState.position = WindowPosition(windowState.position.x + 5.dp, windowState.position.y)
-                        true
-                    }
-
-                    Key.DirectionLeft -> {
-                        windowState.position = WindowPosition(windowState.position.x - 5.dp, windowState.position.y)
-                        true
-                    }
-
-                    Key.DirectionDown -> {
-                        windowState.position = WindowPosition(windowState.position.x, windowState.position.y + 5.dp)
-                        true
-                    }
-
-                    Key.DirectionUp -> {
-                        windowState.position = WindowPosition(windowState.position.x, windowState.position.y - 5.dp)
-                        true
-                    }
-
-                    else -> false
-                }
-
-
-            }
+            focusable = false
         ) {
 
             val viewModel: MainViewModel = viewModel()
             val state by viewModel.state.collectAsStateWithLifecycle(MainState.Loading)
 
-            ContextMenuArea(items = {
-                listOf(ContextMenuItem("Перемещение ${viewModel.isMovable}") {
-                    viewModel.isMovable = !viewModel.isMovable
-                })
-            }) {
-
-                Column {
-                    if (viewModel.isMovable) {
-                        WindowDraggableArea {
-                            Box(
-                                modifier = Modifier.fillMaxWidth().height(10.dp)
-                                    .background(MaterialTheme.colorScheme.primary)
-                            )
-                        }
-                    }
 
 
-                    when (state) {
-                        MainState.Loading -> LoadingScreen()
+            WindowDraggableArea {
 
-                        is MainState.ShowContent -> {
-                            val content = (state as MainState.ShowContent)
-                            LaunchedEffect(state){
-                               if (content.visible){
-                                   windowState.size =  DpSize(300.dp, 50.dp)
-                               }else{
-                                   windowState.size = DpSize.Zero
-                               }
+
+                when (state) {
+                    MainState.Loading -> LoadingScreen()
+
+                    is MainState.ShowContent -> {
+                        val content = (state as MainState.ShowContent)
+                        LaunchedEffect(state) {
+                            if (content.visible) {
+                                windowState.size = DpSize(300.dp, 50.dp)
+                            } else {
+                                windowState.size = DpSize.Zero
                             }
-                            ShowContent(content.text)
                         }
-
-                        is MainState.WaitText -> CodeScreen((state as MainState.WaitText).roomCode)
+                        ShowContent(content.text)
                     }
-                }
 
+                    is MainState.WaitText -> CodeScreen((state as MainState.WaitText).roomCode)
+                }
             }
+
         }
     }
 }
