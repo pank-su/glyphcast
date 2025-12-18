0277ad367a00274feb8e6dc169f1fd9222f99ff5
pank-su <pank@pank.su>
2025-12-18 15:53:58 +0300

    🔧 обновление зависимости tgbotapi до версии 30.2.2

--- tgbot/build.gradle.kts
+++ tgbot/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 dependencies{
     implementation(platform("io.github.jan-tennert.supabase:bom:3.2.6"))
 
-    implementation("dev.inmo:tgbotapi:23.1.0")
+    implementation("dev.inmo:tgbotapi:30.2.2")
     implementation(projects.shared)
     implementation("io.github.jan-tennert.supabase:realtime-kt")
 
